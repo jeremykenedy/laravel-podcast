436682475e793855c48ddfba91b247ef95ed560e
David Egan <dwe@eganrm.com>
2017-11-10 01:42:47 -0800

    added routes

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,27 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+// Auth Routes
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// APP Routes Below
+Route::get('/', 'PodcastsController@index');
+Route::get('/podcast', 'PodcastsController@index');
+Route::get('/podcasts/player', 'PodcastsController@index');
+Route::get('/podcasts/manage', 'PodcastsController@manage');
+Route::get('/podcasts/favorites', 'PodcastsController@favorites');
+Route::get('/podcasts/settings', 'PodcastsController@settings');
+
+Route::get('/podcasts/auto-update', function () {
+	$exitCode = Artisan::call('updatePodcastItems');
+	if ($exitCode == 0) {
+		return redirect('podcasts/player');
+	}
+});
+
+Route::resource('/podcasts', 'PodcastsController');
+
+Route::get('/podcast/search', 'PodcastItemsController@search');
+Route::post('/podcast/mark-as-read', 'PodcastItemsController@markAsRead');
+Route::post('/podcast/mark-as-favorite', 'PodcastItemsController@markAsFavorite');
+Route::post('/podcast/mark-all-prev-read', 'PodcastItemsController@markAllPrevAsRead');
