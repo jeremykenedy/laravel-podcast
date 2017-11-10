023aac1b259bf9c5da2d7291ef4e5bde23174958
David Egan <dwe@eganrm.com>
2017-11-10 01:40:39 -0800

    Added composer requirements
    Included providers and  aliases 
    published illvincent/feeds config file

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        willvincent\Feeds\FeedsServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Feeds'    => willvincent\Feeds\Facades\FeedsFacade::class,
 
     ],
 
